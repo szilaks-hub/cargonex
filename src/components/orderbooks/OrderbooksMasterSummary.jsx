4c661dcd6b87e9c4b1383a1b48b41afcd5c7abb9
Sándor Szilák <szilaks@gmail.com>
2026-02-20 08:28:55 +0000

    File changes

--- src/components/orderbooks/OrderbooksMasterSummary.jsx
+++ src/components/orderbooks/OrderbooksMasterSummary.jsx
@@ -3,6 +3,7 @@ import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import OrderbookDapCalc from "./OrderbookDapCalc";
 
 const ACTIVE_STATUSES = ["booked", "scheduled", "loaded", "in_transit", "customs", "transit", "arrived", "arrived_onsite", "arrived_offsite"];
 
